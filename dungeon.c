336e7b7d7b0f963387a703cf4b7c484e48efec1c
Frank <franks1@iastate.edu>
2021-02-10 22:30:32 -0600

    room generation works, still have overlap

--- dungeon.c
+++ dungeon.c
@@ -3,48 +3,24 @@
 #include <time.h>
 #include "dungeon.h"
 
-
-#define DUNGEONX 80
-#define DUNGEONY 21
-#define MINROOMX 4
-#define MINROOMY 3
-#define MAXROOMX 12
-#define MAXROOMY 9
-
-struct room {
-  int locationX;
-  int locationY;
-  int sizeX;
-  int sizeY;
-};
-
-struct stone{
-  int locationX;
-  int locationY;
-  int hardness;
-};
-
-
 int main(){
   // arrays are row major
   int table[DUNGEONY][DUNGEONX];
+  // TODO: initializes table values to 0, make this look pretty? idk
+  for (int i = 0; i < DUNGEONY; i++) {
+    for (int j = 0; j < DUNGEONX; j++) {
+      table[i][j] = 0;
+    }
+  }
   srand(time(NULL));
   // TODO: determine optimal value for numRooms
   // for now, we'll keep this at the minimum of 6
   int numRooms = 6;
   struct room rooms[numRooms];
-  int i;
-  for(i = 0; i < numRooms; i++){
-    // rooms cannot occupy the perimeter of the dungeon
-    // TODO: validate these values
-    rooms[i].locationY = rand()%(DUNGEONY - 2) + 1;
-    rooms[i].locationX = rand()%(DUNGEONX - 2) + 1;
-    
-    // max sizeY value is 11
-    rooms[i].sizeY = rand()%MAXROOMY + MINROOMY;
-    // max sizeX value is 15
-    rooms[i].sizeX = rand()%MAXROOMX + MINROOMX;
-    
+  
+  initDungeon(rooms, numRooms);
+  
+  for(int i = 0; i < numRooms; i++){
     // this should assign floor values to the table
     for (int j = rooms[i].locationY; j < rooms[i].locationY + rooms[i].sizeY; j++) {
       for (int k = rooms[i].locationX; k < rooms[i].locationX + rooms[i].sizeX; k++) {
@@ -54,6 +30,37 @@ int main(){
   }
   
   printTable(table);
+}
+
+// initializes a dungeon with a set number of rooms
+void initDungeon(struct room *rooms, int numRooms) {
+  int pass = 0; //sentinel value for if the dungeon is valid
+  
+  // loops until a valid combination is found
+  while(!pass) {
+    pass = 1;
+  
+    for(int i = 0; i < numRooms; i++){
+      // rooms cannot occupy the perimeter of the dungeon
+      // TODO: validate these values
+      rooms[i].locationY = rand()%(DUNGEONY - 2) + 1;
+      rooms[i].locationX = rand()%(DUNGEONX - 2) + 1;
+      
+      // max sizeY value is 11
+      rooms[i].sizeY = rand()%MAXROOMY + MINROOMY;
+      // max sizeX value is 15
+      rooms[i].sizeX = rand()%MAXROOMX + MINROOMX;
+      
+      // TODO: remove this, for debugging purposes
+      printf("Room %d: y = %d, x = %d, sizeY = %d, sizeX = %d\n", i, rooms[i].locationY, rooms[i].locationX, rooms[i].sizeY, rooms[i].sizeX);
+      
+      // check if the rooms exceed the bounds of the dungeon (border must be rock)
+      if ((rooms[i].locationY + rooms[i].sizeY > DUNGEONY - 2) || (rooms[i].locationX + rooms[i].sizeX > DUNGEONX - 2)) {
+        pass = 0;
+        break;
+      }
+    }
+  }
 }
 
 // for now, these are the values:
--- dungeon.h
+++ dungeon.h
@@ -1,1 +1,22 @@
+#define DUNGEONX 80
+#define DUNGEONY 21
+#define MINROOMX 4
+#define MINROOMY 3
+#define MAXROOMX 12
+#define MAXROOMY 9
+
+struct room {
+  int locationX;
+  int locationY;
+  int sizeX;
+  int sizeY;
+};
+
+struct stone{
+  int locationX;
+  int locationY;
+  int hardness;
+};
+
+void initDungeon(struct room *rooms, int numRooms);
 void printTable(int table[21][80]);
