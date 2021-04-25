7a0d127b5cbaf430e658c6984810609d16d7593b
Frank <franks1@iastate.edu>
2021-04-24 19:35:55 -0500

    paddle?

--- ball.h
+++ ball.h
@@ -10,7 +10,11 @@ class PongBall {
     PongBall(int x, int y, int z, int a, int q);   
     PongBall();   
 };
+<<<<<<< HEAD
 PongBall::PongBall(){   
+=======
+PongBall::PongBall();    
+>>>>>>> 1df157d... paddle?
 	x = 40;
 	y = 10;
 	speed = 3;
@@ -18,7 +22,11 @@ PongBall::PongBall(){
 	directionY = 1;
 }
 
+<<<<<<< HEAD
 PongBall::PongBall(int xx, int yy, int z, int a, int q){    
+=======
+PongBall::PongBall(int xx, int yy, int z int a, int q);    
+>>>>>>> 1df157d... paddle?
 	x = xx;
 	y = yy;
 	speed = z;
@@ -26,6 +34,7 @@ PongBall::PongBall(int xx, int yy, int z, int a, int q){
 	directionY = q;
 }
 
+<<<<<<< HEAD
 void ball_reverse_x(PongBall *b);
 void ball_reverse_y(PongBall *b);
 int ball_get_y(const PongBall *b);
@@ -33,3 +42,6 @@ int ball_get_x(const PongBall *b);
 int ball_get_speed(const PongBall *b);
 void ball_bounce(PongBall *b);
 void ball_move(PongBall *b);
+=======
+
+>>>>>>> 1df157d... paddle?
--- io.h
+++ io.h
@@ -1,2 +1,5 @@
 void io_initialzie(void);
+<<<<<<< HEAD
 void io_draw(void);
+=======
+>>>>>>> 1df157d... paddle?
